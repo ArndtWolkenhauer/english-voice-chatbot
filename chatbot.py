8a6f9510d186a764761731a083bc44bafdc5ed30
ArndtWolkenhauer <arndt.wolkenhauer@gmx.de>
2025-11-05 08:22:31 +0100

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -15,7 +15,7 @@ text_options = {
     "0_Summer_Vacation_Paris": "https://raw.githubusercontent.com/ArndtWolkenhauer/texts/main/Summer_Vacation_Paris",
     "1_A_New_Colleague": "https://raw.githubusercontent.com/ArndtWolkenhauer/texts/main/1_A_New_Colleague.txt",
     "2_Fundamentals_of_Measuring_Technique": "https://raw.githubusercontent.com/ArndtWolkenhauer/texts/main/2_Fundamentals_of_Measuring_Technique.txt",
-    "3_Measuring_Errors": "https://raw.githubusercontent.com/ArndtWolkenhauer/texts/main/3_Measuring_Errors.txt"
+    "3_Measuring_Errors": "https://raw.githubusercontent.com/ArndtWolkenhauer/texts/main/3_Measuring_Errors.txt",
     "4_Length_Measuring_Instruments": "https://raw.githubusercontent.com/ArndtWolkenhauer/texts/main/4_Length_Measuring_Instruments.txt"
 }
 
